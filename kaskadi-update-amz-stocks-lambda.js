a5ae6ac1c388326f08a36fa0be059a76cf156b06
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-05-19 13:29:25 +0200

    Rewriting lambda for AMZ warehouses

--- kaskadi-update-amz-stocks-lambda.js
+++ kaskadi-update-amz-stocks-lambda.js
@@ -1,6 +1,4 @@
 const AWS = require('aws-sdk')
-const WemaloClient = require('wemalo-api-wrapper')
-const client = new WemaloClient({token: process.env.WEMALO_TOKEN})
 const lambda = new AWS.Lambda({region: 'eu-central-1'})
 const es = require('aws-es-client')({
   id: process.env.ES_ID,
@@ -9,30 +7,52 @@ const es = require('aws-es-client')({
 })
 
 module.exports.handler = async (event) => {
-  const lastUpdated = (await es.get({
-    id: 'ysws',
-    index: 'warehouses'
-  }))._source.stockLastUpdated
-  const stocks = await getStocksData(lastUpdated)
-  await setStockData(stocks)
-  return {
+  let ids
+  let res = {
     statusCode: 200,
     headers: {
       'Access-Control-Allow-Origin': '*'
-    },
-    body: JSON.stringify({
-      stocks
-    })
+    }
   }
+  if (event.queryStringParameters) {
+    const countryCode = event.queryStringParameters.code
+    if (!countryCode) {
+      res.statusCode = 400
+      res.body = JSON.stringify({ message: `Please provide a warehouse country code in your query string.` })
+      return res
+    }
+    ids = [countryCode.toLowerCase()]
+  } else {
+    ids = Object.keys(require('./marketplaces.js')).map(key => key.toLowerCase())
+  }
+  res.body = JSON.stringify(await updateStocks(ids))
+  return res
 }
 
-async function setStockData(stocks) {
+async function updateStocks (ids) {
+  let stockMap = {}
+  for (const id of ids) {
+    const warehouse = await es.get({
+      id: `amz_${id}`,
+      index: 'warehouses'
+    })
+    if (warehouse.found) {
+      const lastUpdated = warehouse._source.stockLastUpdated || 1262300400000 // default to 01/01/2015
+      const stocks = await getStocksData(lastUpdated)
+      stockMap[warehouse] = stocks
+      await setStockData(stocks, warehouse)
+    }
+  }
+  return stockMap
+}
+
+async function setStockData(stocks, warehouse) {
   if (stocks.length === 0) {
     return
   }
   const event = {
     stockData: stocks,
-    warehouse: 'ysws'
+    warehouse
   }
   await lambda.invoke({
     FunctionName: 'kaskadi-update-stocks-lambda',
