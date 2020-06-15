d9964e59d5ee3e0deb9956cac890229ded7efff4
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-06-15 19:42:54 +0200

    Hotfix: removed remains of old code (extra event)

--- helpers/get-country-ids.js
+++ helpers/get-country-ids.js
@@ -1,6 +1,6 @@
 module.exports = (queryStringParameters) => {
   if (queryStringParameters) {
-    const countryCode = event.queryStringParameters.code
+    const countryCode = queryStringParameters.code
     return countryCode ? [countryCode.toLowerCase()] : []
   }
   return Object.keys(require('../marketplaces.js')).map(key => key.toLowerCase())
