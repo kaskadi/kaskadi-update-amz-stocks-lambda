20ec4f31f615e02ac207d573e45ad1b002d2f620
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-06-16 11:35:05 +0200

    Hotfix: fixed filtering logic

--- helpers/filter-stocks.js
+++ helpers/filter-stocks.js
@@ -1,21 +1,22 @@
 // we need to filter our stocks data because over time some products saw their SKUs edited manually to be able to relist product while reusing EAN/ASIN. This means that only products whose SellerSKU ends with "-new" (non-case sensitive!) are the ones for which we should consider stock changes for a given EAN/ASIN
 module.exports = (stocks) => {
-  return stocks.map(filterStockData).map(removeSkuField)
+  return stocks.map(removeDuplicatedStock).map(removeSkuField)
 }
 
-function filterStockData (stock) {
-    const rawStockData = stock.stockData
-    return {
-      ...stock,
-      stockData: rawStockData.filter(stockDataFilterHandler(rawStockData))
-    }
+function removeDuplicatedStock (stock) {
+  const stockData = stock.stockData
+  let ids = stockData.map(data => data.id)
+  ids = [...new Set(ids)] // remove duplicated ids
+  return {
+    ...stock,
+    stockData: ids.map(getStockDataForId(stockData))
+  }
 }
 
-function stockDataFilterHandler (stockData) {
-  return data => {
-    const dataOccurences = stockData.filter(stock => stock.id === data.id)
-    const sku = data.sku
-    return dataOccurences.length === 1 ? true : sku[sku.length - 4].toLowerCase() === '-new' 
+function getStockDataForId (stockData) {
+  return id => {
+    const dataOccurences = stockData.filter(stock => stock.id === id)
+    return dataOccurences.length === 1 ? dataOccurences[0] : dataOccurences.filter(data => data.sku[data.sku.length - 4].toLowerCase() === '-new') 
   }
 }
 
